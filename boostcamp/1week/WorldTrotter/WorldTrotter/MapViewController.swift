e1bced87893c4fa7ee245884872c21e53e989980
JINHYUCK-r <wlsgur7701@naver.com>
2020-08-26 17:25:18 +0900

    탭뷰/맵뷰/맵키트설치

---
+++ boostcamp/1week/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by 류진혁 on 2020/08/26.
+//  Copyright © 2020 Jin-Hyuck. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("MapViewController loaded its view")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- boostcamp/1week/WorldTrotter/WorldTrotter/ViewController.swift
+++ boostcamp/1week/WorldTrotter/WorldTrotter/ViewController.swift
@@ -18,6 +18,27 @@ class ViewController: UIViewController,UITextFieldDelegate {
           }
       }
     
+   override func viewDidLoad() {
+            super.viewDidLoad()
+            print("controllerView loaded its view")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let view = self.view
+        var currentTime = String()
+        let nowDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH"
+        dateFormatter.locale = Locale(identifier: "ko_Kr")
+        currentTime = dateFormatter.string(from: nowDate)
+        
+        if(Int(currentTime)!<6) || (Int(currentTime)!>18){
+            view?.backgroundColor = UIColor.darkGray
+        }else{
+            view?.backgroundColor = UIColor.cyan
+        }
+    
+    }
+    
     var celsiusValue : Double?{
         if let value = fahrenheitValue{
             return (value-32)*(5/9)
@@ -56,6 +77,8 @@ class ViewController: UIViewController,UITextFieldDelegate {
    //textField 입력 값의 범위를 소수 첫째 자리만 받음
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        
+        //알파벳 문자 입력 받지 않게 하기
         let replacementTextHasDecimalSeparator = string.range(of: ".")
         let hasAlphabet = string.rangeOfCharacter(from: NSCharacterSet.letters) // 알파벳을 가지고 있으면 거기에 맞는 range 반환.
         
