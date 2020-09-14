754ca15bbe0fe78aa5b796e341467563d41db9b1
JINHYUCK-r <wlsgur7701@naver.com>
2020-09-14 16:18:16 +0900

    TableView에 DispatchQueue 적용

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -26,11 +26,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.textLabel?.text = friend.name.full
         cell.detailTextLabel?.text = friend.email
+        // 셀이 재사용되기 전에 초기화 시켜주어야함. 나중에 이미지를 가져오기전에 다른사람의 이미지를 표현하지 않게 하기 위해
+        cell.imageView?.image = nil
         
+        /*
         //문제가 있는코드
         guard let imageURL: URL = URL(string: friend.picture.thumbnail) else{ return cell}
+        //imagaData는 동기메소드이기때문에 이미지를 전부 불러올때까지 화면이 멈추게 됨
         guard let imageData : Data = try? Data(contentsOf: imageURL) else{return cell}
-        cell.imageView?.image = UIImage(data: imageData)
+        */
+        //백그라운드에서 작업. 여기서 return cell을 하면 오류가 생김.
+        DispatchQueue.global().async {
+            guard let imageURL: URL = URL(string: friend.picture.thumbnail) else{ return }
+             guard let imageData : Data = try? Data(contentsOf: imageURL) else{return }
+            
+           //작업된 결과를 메인스레드로 가져와서 동작
+            DispatchQueue.main.async {
+                //사용자가 화면을 움직이게되면 셀의 번호가 달라지기때문에 오류가 발생할수있음. 화면에 보여지는 셀의 인덱스를 번호를 맞춰주기 위한 코드, 서로 일치하는 상황에서만 구현
+                if let index : IndexPath = tableView.indexPath(for: cell){
+                    if index.row == indexPath.row{
+                        cell.imageView?.image = UIImage(data: imageData)
+                    }
+                }
+                
+                
+            }
+        }
+        //cell.imageView?.image = UIImage(data: imageData)
         
         return cell
         
@@ -50,7 +72,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         //세션생성
         let session : URLSession = URLSession(configuration: .default)
-        //데이터테스크 생성. url로 요청/ { 뒤에 클로저는 요청에 대한 서버의 응답이 왓을때 호출될 클로저. 서버에서 보내준 데이터. 그곳의 응답. 오류가 발생하면
+        //데이터테스크 생성. url로 요청/ { 뒤에 클로저는 요청에 대한 서버의 응답이 왓을때 호출될 클로저. 서버에서 보내준 데이터. 그곳의 응답. 오류가 발생하면/ 클로저는 백그라운드에서 작업됨
         let datatask : URLSessionDataTask = session.dataTask(with: url) { (data: Data?, reponse: URLResponse?, error : Error?) in
             if let error = error {
                 print(error.localizedDescription)
@@ -63,7 +85,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 let apiResponse : APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
                 self.friends = apiResponse.results
                 //reloadData는 메인스레드에서 동작해주어야 하는 지금 클로저는 메인스레드에서 동작하지않음/ 많은 데이터가 올시 오류가 생길수 있으니 백그라운드에서 동작하도록 수정해주어야함
-                self.tableVIew.reloadData()
+                 //self.tableVIew.reloadData()
+                //메인스레드로 가져가야할 코드는 dispatchqueue를 사용
+                DispatchQueue.main.async {
+                    self.tableVIew.reloadData()
+                }
+               
             }catch(let err){
                 print(err.localizedDescription)
             }
