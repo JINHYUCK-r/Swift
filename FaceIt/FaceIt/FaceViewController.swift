0c0b9e7e1d440d904c7c417235bffece7decfda7
JINHYUCK-r <wlsgur7701@naver.com>
2021-07-02 21:21:47 +0900

    FaceIt

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by 류진혁 on 2021/07/02.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
